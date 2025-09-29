3ab8f5ae2040c806d558f448a877732c4d9794dc
Juan Arvizu <arzzdeveloper04@gmail.com>
2025-09-29 12:30:45 -0600

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: process.env.DB_SSL_CA ? { ca: process.env.DB_SSL_CA } : undefined
+  ssl: process.env.DB_CA
 });
 
 export default pool;
