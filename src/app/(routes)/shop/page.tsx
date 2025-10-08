e0af4f9e1be0472907c3cdf5230831f99cac5f7b
Juan Arvizu <arzzdeveloper04@gmail.com>
2025-10-08 00:38:30 -0600

    Create page.tsx

---
+++ src/app/(routes)/shop/page.tsx
@@ -0,0 +1,7 @@
+export default function BusinessPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-3xl font-bold">AQUI SE HACE LA TIENDA</h1>
+    </div>
+  );
+}
